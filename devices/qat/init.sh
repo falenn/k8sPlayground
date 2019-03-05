a357170cbf0876a37edab7fd5a5900ba692a2265
dldvcs <dlehner@meadowgate.us>
2019-03-05 12:13:14 -0500

    nginx successfully offloaded to the card from a k8s pod

--- devices/qat/init.sh
+++ devices/qat/init.sh
@@ -4,6 +4,8 @@ cd /usr/local/nginx/ssl/certs/
 
 /usr/local/ssl/bin/openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout server.key -out server.crt -extensions san -config <(echo "[req]"; echo distinguished_name=req; echo "[san]"; echo subjectAltName=DNS:example.com,DNS:example.net,IP.1:127.0.0.1,IP.2:$(hostname -i)) -subj /CN=example.com
 
+ulimit -l 2048
+
 /usr/local/nginx/sbin/nginx &
 iptables -t nat -A PREROUTING -p tcp --dport 1:65535 -j REDIRECT --to-ports 443
 iptables -t nat -A PREROUTING -p udp --dport 1:65535 -j REDIRECT --to-ports 443

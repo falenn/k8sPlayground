6235ca02a913425b58fbca2d712374e2f745a34b
falenn <dev@imwiz.com>
2020-12-15 19:15:24 -0500

    updated install with weave

--- scripts/installK8s.sh
+++ scripts/installK8s.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 POD_CIDR="10.244.0.0/16"
+INIT_SCRIPT=kubeadm-init2-config.yaml
+sudo rm /etc/sysctl.d/k8s.conf
+#sudo touch /etc/sysctl.d/k8s.conf
 
 sudo cat <<EOF | sudo tee -a /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sudo sysctl --system
+
+function installCNI {
+echo "CNI function"
+# install Calico
+
+#curl https://docs.projectcalico.org/manifests/calico.yaml -O
+#sed -i -e "s?192.168.0.0/16?$POD_CIDR?g" calico.yaml
+
+#kubectl apply -f calico.yaml
+
+
+# install flannel
+# curl -LJO https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
+# sudo kubectl apply -f kube-flannel.yml
+
+}
 
 
 sudo cat <<EOF | sudo tee -a /etc/yum.repos.d/kubernetes.repo
@@ -34,11 +53,9 @@ sudo systemctl enable kubelet && sudo systemctl start kubelet
 sudo kubeadm reset
 
 # initialize master and set network CIDR for CNI
-sudo kubeadm init --pod-network-cidr=$POD_CIDR
+# sudo kubeadm init --pod-network-cidr=$POD_CIDR
+sudo kubeadm init --config $INIT_SCRIPT
 
-# install flannel
-# curl -LJO https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
-# sudo kubectl apply -f kube-flannel.yml
 
 # setup kubectl for non-root user
 mkdir -p $HOME/.kube
@@ -55,16 +72,14 @@ while [ $READY -ne 0 ]; do
 done
 
 
-# install Calico
-curl https://docs.projectcalico.org/manifests/tigera-operator.yaml -O
-curl https://docs.projectcalico.org/manifests/custom-resources.yaml -O
-sed -i -e "s?192.168.0.0/16?$POD_CIDR?g" custom-resources.yaml
-sed -i -e "s?192.168.0.0/16?$POD_CIDR?g" tigera-operator.yaml
-
-kubectl apply -f tigera-operator.yaml
-kubectl apply -f custom-resources.yaml
-
-
 #allow scheduling on the master
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# Install CNI
+
+installCNI
+
+
+
+
 
